b51d29d63699fa04198ed5833416e96a253cff14
Mark Topper <mail@webman.io>
2016-06-08 22:51:00 +0200

    Add enableAutocorrect and disableAutocorrect

--- System/Profile/Aliases.sh
+++ System/Profile/Aliases.sh
@@ -12,3 +12,9 @@ alias showHiddenFiles='defaults write com.apple.finder AppleShowAllFiles YES; ki
 
 # Hide hidden files
 alias hideHiddenFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
+# Enable autocorrect
+alias enableAutocorrect='defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool true'
+
+# Disable autocorrect
+alias disableAutocorrect='defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false'
