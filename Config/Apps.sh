2b7e7cd7f0637c177d9f4f3564348cba3875b138
Mark Topper <mail@webman.io>
2016-06-08 22:17:11 +0200

    Add Google Chrome

--- Config/Apps.sh
+++ Config/Apps.sh
@@ -2,4 +2,4 @@
 apps=(1password anvil atom firefox battle-net bartender dropbox \
  github-desktop iterm2 mamp opera parallels-desktop phpstorm postbox \
  skype sonos sourcetree spotify steam teamviewer transmit vagrant \
- vagrant-manager versions wd-my-cloud virtualbox)
+ vagrant-manager versions wd-my-cloud virtualbox google-chrome)
