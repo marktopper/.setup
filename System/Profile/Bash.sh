344cfb2a42145bed7ef6fe7f1d7d3773e5f7788e
Mark Topper <marktopperdk@gmail.com>
2016-06-10 21:49:17 +0200

    Add new aliases
    New aliases:
    - art
    - home
    - hosts
    - reload
    - sim
    - docs
    - c
    - lookbusy
    - morning
    - gitmessage
    - online
    - ipinfo
    - ip
    - vag

--- System/Profile/Bash.sh
+++ System/Profile/Bash.sh
@@ -8,3 +8,18 @@ source ~/.setup/System/Profile/Bash/Heroku.sh
 source ~/.setup/System/Profile/Bash/Node.sh
 #source ~/.setup/System/Profile/Bash/Ruby.sh
 source ~/.setup/System/Profile/Bash/XQuartz.sh
+
+alias art='php artisan'
+alias home='cd ~/'
+alias hosts='sudo nano /etc/hosts'
+alias reload='source ~/.bash_profile'
+alias sim='open /Applications/Xcode.app/Contents/Applications/iOS Simulator.app'
+alias docs='cd ~/Documents'
+alias c='clear'
+alias lookbusy='cat/dev/urandom | hex dump -C | grep —color "ca fe"'
+alias morning='open spotify:track:6Z1Bm6R5I3Kc38ycsj6pgk'
+alias gitmessage='curl -s http://whatthecommit.com/index.txt'
+alias online='echo "Ping Google.com" && ping google.com'
+alias ipinfo='curl ipinfo.io'
+alias ip='curl icanhazip.com'
+alias vag='cd ~/.homestead; vagrant'
