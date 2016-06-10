8d105d642c7af735525085b2308d2cf0da4879d5
Mark Topper <marktopperdk@gmail.com>
2016-06-10 22:08:43 +0200

    New bash aliases
    New aliases:
    - flush
    - afk
    - showdesktop
    - hidedesktop
    - cleanup
    - emptytrash
    - bu (Update everything installed using Homebrew)
    - list-users

--- System/Profile/Bash.sh
+++ System/Profile/Bash.sh
@@ -23,3 +23,55 @@ alias online='echo "Ping Google.com" && ping google.com'
 alias ipinfo='curl ipinfo.io'
 alias ip='curl icanhazip.com'
 alias vag='cd ~/.homestead; vagrant'
+alias flush="dscacheutil -flushcache && killall -HUP mDNSResponder"
+alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
+alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl; sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'"
+
+boop() {
+  brew update && brew upgrade && brew cleanup \
+    && cask-upgrade && cask-retire && brew cask cleanup \
+      && brew doctor && brew cask doctor
+}
+
+__is_pinned() {
+  local pinned=";pixate-studio;sketchup;skype;"
+  [[ *";${1};"* != "${pinned}" ]] && return 0 || return 1
+}
+
+cask-upgrade() {
+  while read -r; do
+    reply=(${REPLY})
+    name="${reply[0]}"
+    vers="${reply[1]}"
+    if ! __is_pinned "${name}"; then
+      if [[ "${vers}" == "latest" ]]; then
+        brew cask install "${name}" --force
+      else
+        brew cask install "${name}" 2> >(grep -v "re-install")
+      fi
+    fi
+  done < <(brew cask list --versions)
+}
+
+cask-retire() {
+  room="/opt/homebrew-cask/Caskroom"
+  while read -r; do
+    reply=(${REPLY})
+    name="${reply[0]}"
+    vers="${reply[1]}"
+    if ! __is_pinned "${name}"; then
+      for inst in $(ls "${room}/${name}"); do
+        if [[ "${inst}" != "${vers}" ]]; then
+          echo "Removing ${name} version ${inst}"
+          rm -r "${room}/${name}/${inst}"
+        fi
+      done
+    fi
+  done < <(brew cask list --versions)
+}
+alias bu="boop"
+
+alias list-users="dscl . list /Users | grep -v ^_.*"
