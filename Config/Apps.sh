bb4abdc696b07a38fdca845259e2213142db64f7
Mark Topper <marktopperdk@gmail.com>
2016-06-13 16:00:04 +0200

    Commented out apps by default
    Commented out apps by default, required apps are still pre-selected for
    installation.

--- Config/Apps.sh
+++ Config/Apps.sh
@@ -1,30 +1,31 @@
 # Define cask applications to install
 apps=(\
- 1password \
- anvil \
- atom \
- firefox \
- battle-net \
- bartender \
- dropbox \
- github-desktop \
- iterm2 \
- mamp \
- opera \
- parallels-desktop \
- phpstorm \
- postbox \
- skype \
- sonos \
- sourcetree \
- spotify \
- steam \
- teamviewer \
- transmit \
+ #1password \
+ #anvil \
+ #atom \
+ #firefox \
+ #battle-net \
+ #bartender \
+ #dropbox \
+ #github-desktop \
+ #google-chrome \
+ #iterm2 \
+ #mamp \
+ #opera \
+ #parallels-desktop \
+ #phpstorm \
+ #postbox \
+ #skype \
+ #sonos \
+ #sourcetree \
+ #spotify \
+ #steam \
+ #sublime-text3 \
+ #teamviewer \
+ #transmit \
  vagrant \
- vagrant-manager \
- versions \
- wd-my-cloud \
+ #vagrant-manager \
+ #versions \
+ #wd-my-cloud \
  virtualbox \
- google-chrome \
 )
