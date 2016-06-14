f7727cd35aa606d4b2c82622b7dcc9cc8563beea
Mark Topper <marktopperdk@gmail.com>
2016-06-14 15:06:06 +0200

    Support Homebrew Path Update

--- System/Install/Homebrew.sh
+++ System/Install/Homebrew.sh
@@ -5,7 +5,16 @@ if ! command_exists brew; then
   brew doctor
   echo "=== Homebrew installed."
 fi
-brew -v
+#brew -v
 #sudo chown -R $USER /usr/local/Cellar
 brew update
+brew cleanup
+brew cask cleanup
 echo "=== Updated homebrew bottles."
+
+# Update homebrew path if not updated
+if [[ -e /opt/homebrew-cask/Caskroom ]]; then
+  echo "=== Updating homebrew path..."
+  mv /opt/homebrew-cask/Caskroom /usr/local/Caskroom
+  echo "=== Homebrew path updated."
+fi
