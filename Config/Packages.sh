1786fe3cef016e3eebf0cc4210293d8c0e094139
Mark Topper <marktopperdk@gmail.com>
2016-06-15 22:01:00 +0200

    Use updated php-cs-fixer
    Use friendsofphp/php-cs-fixer over fabpot/php-cs-fixer

--- Config/Packages.sh
+++ Config/Packages.sh
@@ -3,8 +3,8 @@ INSTALL_PACKAGES=true
 
 # Define composer packages to install
 packages=(\
- fabpot/php-cs-fixer \
  laravel/installer \
  laravel/spark-installer \
  laravel/valet \
+ friendsofphp/php-cs-fixer \
 )
