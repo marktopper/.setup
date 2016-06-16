a8643c9dc9cf8e739b0e8aeb93785aadf40e8eb3
Mark Topper <marktopperdk@gmail.com>
2016-06-16 12:44:21 +0200

    Spelling

--- System/Install/Valet.sh
+++ System/Install/Valet.sh
@@ -2,7 +2,7 @@ if [[ $INSTALL_VALET == true ]]; then
   valet install
 fi
 
-if [ -d ~/.composer/vendor/laravel/valet ]; thenn
+if [ -d ~/.composer/vendor/laravel/valet ]; then
   if [[ $CONFIGURE_VALET ]]; then
     [ -d $VALET_PATH ] || mkdir $VALET_PATH
 
