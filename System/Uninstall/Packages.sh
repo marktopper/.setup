bfd6af0cea1cc47305cdf845955e776be0d045cf
Mark Topper <marktopperdk@gmail.com>
2016-07-17 15:13:52 +0200

    Check before uninstall
    Check before uninstalling composer package

--- System/Uninstall/Packages.sh
+++ System/Uninstall/Packages.sh
@@ -1,8 +1,12 @@
 # Uninstall composer package if installed
 function uninstall_package {
-  echo "=== Uninstalling $1..."
-  composer global remove $1
-  echo "=== $1 uninstalled."
+  if [ -d ~/.composer/vendor/$1 ]; then
+    print_header "Uninstalling $1..."
+    composer global remove $1
+    print_header "$1 uninstalled."
+  else
+    print_header "Skipping $1, already uninstalled."
+  fi
 }
 
 # Uninstall all composer packages
