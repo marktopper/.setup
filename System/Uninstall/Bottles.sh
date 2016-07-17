aaffe940d3a57f2ae7cea20014db6737598872e5
Mark Topper <marktopperdk@gmail.com>
2016-07-17 15:16:16 +0200

    Fixed header

--- System/Uninstall/Bottles.sh
+++ System/Uninstall/Bottles.sh
@@ -1,11 +1,11 @@
 # Uninstall brew bottle if command does not exists
 function uninstall_bottle {
-  if command_exists $1; then
-    echo "=== Uninstalling $1..."
+  if [ -d /usr/local/Cellar/$1 ]; then
+    print_header "Uninstalling $1..."
     brew install $1
-    echo "=== $1 uninstalled."
+    print_header "$1 uninstalled."
   else
-    echo "=== Skipping $1, already uninstalled."
+    print_header "Skipping $1, already uninstalled."
   fi
 }
 
