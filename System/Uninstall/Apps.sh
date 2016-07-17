f5de7b9af6697170838d449463bb4383220cf489
Mark Topper <marktopperdk@gmail.com>
2016-07-17 15:15:57 +0200

    Check before uninstall
    Add check before uninstalling cask app

--- System/Uninstall/Apps.sh
+++ System/Uninstall/Apps.sh
@@ -1,8 +1,12 @@
 # Install  app if not installed
 function uninstall_app {
-  echo "=== Uninstalling $1..."
-  brew cask uninstall $1
-  echo "=== $1 uninstalled."
+  if [ -d /usr/local/Caskroom/$1 ]; then
+    print_header "Uninstalling $1..."
+    brew cask uninstall $1
+    print_header "$1 uninstalled."
+  else
+    print_header "Skipping $1, already uninstalled."
+  fi
 }
 
 # Uninstall all cask applications
