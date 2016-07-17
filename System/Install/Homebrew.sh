44ce2227f69445cf92aeb261f6b5bfc1b4ed1992
Mark Topper <marktopperdk@gmail.com>
2016-07-17 15:14:24 +0200

    Cleanup
    Cleanup Homebrew

--- System/Install/Homebrew.sh
+++ System/Install/Homebrew.sh
@@ -5,19 +5,10 @@ if [[ $INSTALL_HOMEBREW ]]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew doctor
     print_header "Homebrew installed."
-  fi
-  #brew -v
-  #sudo chown -R $USER /usr/local/Cellar
-  brew update
-  brew cleanup
-  brew cask cleanup
-  print_header "Updated homebrew bottles."
-
-  # Update homebrew path if not updated
-  # https://github.com/caskroom/homebrew-cask/issues/21913
-  if [[ -e /opt/homebrew-cask/Caskroom ]]; then
-    print_header "Updating homebrew path..."
-    mv /opt/homebrew-cask/Caskroom /usr/local/Caskroom
-    print_header "Homebrew path updated."
+  else
+    brew upgrade
+    brew update
+    brew cleanup
+    brew cask cleanup
   fi
 fi
