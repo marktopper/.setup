d17e843a86681b2afcdf298124ae8ad73b6379ac
Mark Topper <marktopperdk@gmail.com>
2016-07-26 16:01:22 +0200

    Check before uninstall plugin
    Check if vagrant plugin exists before uninstalling it

--- System/Uninstall/Vagrant.sh
+++ System/Uninstall/Vagrant.sh
@@ -4,4 +4,6 @@
 ########################################
 
 # Uninstall Parallels Plugin for Vagrant
-vagrant plugin uninstall vagrant-parallels
+if vagrant plugin list | grep -q '^vagrant-parallels '; then
+  vagrant plugin uninstall vagrant-parallels
+fi
