79062e06fa4eb0f29fc5cb63d87fb6ce058e2a6d
Mark Topper <mark@ulties.com>
2016-08-30 17:34:19 +0200

    Remove RVM

--- System/Install/RVM.sh
+++
@@ -1,9 +0,0 @@
-if [[ $INSTALL_RVM == true ]]; then
-  if ! command_exists rvm; then
-    print_header "Installing RVM..."
-    curl -L https://get.rvm.io | bash -s stable
-    print_header "RVM installed."
-    source ~/.bash_profile
-  fi
-  rvm -v
-fi
