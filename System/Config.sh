e0fd6342cee7d2f3bdb7524c3fdc262ef194189e
Mark Topper <mark@ulties.com>
2016-08-30 17:34:03 +0200

    Load user config

--- System/Config.sh
+++ System/Config.sh
@@ -1,2 +1,12 @@
 # Load all files in Config
 load_files_in Config
+
+# Create UserConfig if not exists
+if [ ! -d ~/.SetupUserConfig ]; then
+    cp -r ~/.setup/Config ~/.SetupUserConfig
+fi
+
+# Load all user config files
+for ENTRY in `ls ~/.SetupUserConfig`; do
+    source ~/.SetupUserConfig/"$ENTRY"
+done
